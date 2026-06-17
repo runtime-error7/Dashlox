b872161353cad102362310f496662e499685077c
Nitin Raj <nrajmrvl@gmail.com>
2026-06-17 13:32:25 +0530

    Change python command to python3 in start.sh
    Updated the script to use python3 for running the main server.

--- start.sh
+++ start.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+
 echo "⚡ Starting Dashlox Setup..."
+
+# Upgrade pip and install the required packages
+python3 -m pip install --upgrade pip
 pip install -r requirements.txt
+
 echo "🚀 Launching Dashlox App..."
-python src/backend/main.py
+
+# Run the main server file
+python3 src/backend/main.py
